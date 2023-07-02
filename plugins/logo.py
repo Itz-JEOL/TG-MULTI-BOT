fdf33b0d70fd39ecd33945f227939d0eced458c1
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-07-02 17:02:23 +0530

    Update logo.py

--- plugins/logo.py
+++ plugins/logo.py
@@ -7,7 +7,8 @@ import requests
 
 @Client.on_message(filters.command("logo"))
 async def logo(bot, msg: Message):
-    text = await msg.reply("Usage:\n\n /logo Jeol")
+    if len(message.command) == 1:
+       return await msg.reply_text("Usage:\n\n /logo Jeol")
     logo_name = msg.text.split(" ", 1)[1]
     await text.delete()
     API = f"https://api.sdbots.tk/logohq?text={logo_name}"
